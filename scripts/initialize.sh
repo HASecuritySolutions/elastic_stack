41c19d763699868b57a3bf9ad30f6b880f3ca439
Justin Henderson <jhenderson@tekrefresh.com>
2018-05-09 22:50:55 -0500

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,5 +3,6 @@ cp -f /opt/elastic_stack/docker-compose.yml.example /opt/elastic_stack/docker-co
 cp -f /opt/elastic_stack/curator/example/* /opt/elastic_stack/curator/
 cp -f /opt/elastic_stack/cron/custom-cron.example /opt/elastic_stack/cron/custom-cron
 cp -f /opt/elastic_stack/elastalert/config.yaml.example /opt/elastic_stack/elastalert/config.yaml
+mkdir /opt/elastic_stack/elastalert/rules
 cp -f /opt/elastic_stack/elastalert/example_rules/* /opt/elastic_stack/elastalert/rules/
 cp -f /opt/elastic_stack/logstash/pipelines.yml.example /opt/elastic_stack/logstash/pipelines.yml
